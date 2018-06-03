0227938774a63c8089409aa13b1ea5b65baa208a
StaceyCoder <Stacey.Beason@gmail.com>
2018-06-03 13:35:45 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const ejs = require('ejs')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const POST = process.env.POST || 3000
+const app = express()
+
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static('public'))
+
+app.use(cookieParser())
+
+app.get('/login', (req, res)=>{ //Get = req.cookies.name of cookies
+  return res.render('login', {username: req.cookies.username})
+})
+
+app.post('/login', (req, res)=>{
+  let username = req.body.username
+  res.cookie('username', username, {maxAge: 90000})
+  return res.redirect('/login')
+})
+// set
+// res.cookies
+// get
+// req.cookies
+
+app.listen(POST, ()=>{
+  console.log('Server running...')
+})
